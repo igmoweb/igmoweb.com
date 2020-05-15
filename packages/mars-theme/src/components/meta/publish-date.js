171f8ecd63ce87735c591d42e83b3e99624bb896
Ignacio Cruz <igmoweb@gmail.com>
2020-05-15 21:42:28 +0200

    Improved date format

--- packages/mars-theme/src/components/meta/publish-date.js
+++ packages/mars-theme/src/components/meta/publish-date.js
@@ -1,5 +1,6 @@
-import { styled } from 'frontity';
 import config from '../../config';
+import { dateI18n } from '@wordpress/date';
+import { styled } from 'frontity';
 
 const { fontSizes } = config;
 
@@ -7,18 +8,11 @@ const Container = styled.div`
 	font-size: ${fontSizes.xsmall};
 `;
 
-const DATE_OPTIONS = {
-	weekday: 'long',
-	year: 'numeric',
-	month: 'long',
-	day: 'numeric',
-};
-
 const PublishDate = ( { postDate } ) => {
 	const date = new Date( postDate );
 	return (
 		<Container className="publish-date">
-			{ date.toLocaleDateString( undefined, DATE_OPTIONS ) }
+			{ dateI18n( 'd M, Y', date.getTime() ) }
 		</Container>
 	);
 };
