1d9788f659ed8cd2e4938d5fda32c4464e51d797
Ignacio Cruz <igmoweb@gmail.com>
2020-05-15 22:53:56 +0200

    Added a nice 404 page

--- packages/igmoweb-theme/src/components/page-error.js
+++ packages/igmoweb-theme/src/components/page-error.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import { connect, styled } from 'frontity';
+import background from '../assets/murray.jpg';
+import { mq } from '../styles';
+import { Global, connect, css, styled } from 'frontity';
 
-const description404 = (
-	<>
-		That page can’t be found{ ' ' }
-		<span aria-label="confused face" role="img">
-			😕
-		</span>
-	</>
-);
+const globalStyles = css`
+	html {
+		background-image: url( ${background} );
+		background-repeat: no-repeat;
+		background-size: auto;
+		${mq( 'medium' )} {
+			background-size: cover;
+		}
+	}
 
-const description = (
-	<>
-		Don&apos;t panic! Seems like you encountered an error. If this persists,
-		<a href="https://community.frontity.org"> let us know </a> or try
-		refreshing your browser.
-	</>
-);
+	body {
+		background: none;
+	}
+
+	#footer,
+	#branding > div {
+		display: none;
+	}
+
+	#navigation {
+		background-color: transparent;
+		justify-content: left;
+		padding-left: 20px;
+	}
+`;
 
 // The 404 page component
 const Page404 = ( { state } ) => {
@@ -27,11 +38,9 @@ const Page404 = ( { state } ) => {
 
 	return (
 		<>
+			<Global styles={ globalStyles } />
 			<Container>
 				<Title>{ data.is404 ? title404 : title }</Title>
-				<Description>
-					{ data.is404 ? description404 : description }
-				</Description>
 			</Container>
 		</>
 	);
@@ -43,7 +52,7 @@ const Container = styled.div`
 	width: 800px;
 	margin: 0;
 	padding: 24px;
-	text-align: center;
+	text-align: left;
 `;
 
 const Title = styled.h1`
@@ -52,10 +61,4 @@ const Title = styled.h1`
 	margin-bottom: 8px;
 	color: rgba( 12, 17, 43 );
 	font-size: 4em;
-`;
-
-const Description = styled.div`
-	line-height: 1.6em;
-	color: rgba( 12, 17, 43, 0.8 );
-	margin: 24px 0;
 `;
