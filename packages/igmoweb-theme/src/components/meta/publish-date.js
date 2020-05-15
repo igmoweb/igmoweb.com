fddf5b216e169ee52970f04da20003c5853ff380
Ignacio Cruz <igmoweb@gmail.com>
2020-05-15 20:44:31 +0200

    Revert "Update date format"
    This reverts commit 3f5d9c313be8ae8ffd87c6257e4c8c91914a28bb.

--- packages/igmoweb-theme/src/components/meta/publish-date.js
+++ packages/igmoweb-theme/src/components/meta/publish-date.js
@@ -1,20 +1,26 @@
-import config from '../../config';
-import { dateI18n } from '@wordpress/date';
 import { styled } from 'frontity';
+import config from '../../config';
 
 const { fontSizes } = config;
+
+const Container = styled.div`
+	font-size: ${fontSizes.xsmall};
+`;
+
+const DATE_OPTIONS = {
+	weekday: 'long',
+	year: 'numeric',
+	month: 'long',
+	day: 'numeric',
+};
 
 const PublishDate = ( { postDate } ) => {
 	const date = new Date( postDate );
 	return (
 		<Container className="publish-date">
-			{ dateI18n( 'd M, Y', date.getTime() ) }
+			{ date.toLocaleDateString( undefined, DATE_OPTIONS ) }
 		</Container>
 	);
 };
 
 export default PublishDate;
-
-const Container = styled.div`
-	font-size: ${fontSizes.xsmall};
-`;
