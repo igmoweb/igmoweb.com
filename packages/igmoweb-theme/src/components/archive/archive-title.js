400fb84cfa6c18c83da56cd8bf8c089606065c43
Ignacio Cruz <igmoweb@gmail.com>
2020-05-16 08:29:25 +0200

    Added search archive

--- packages/igmoweb-theme/src/components/archive/archive-title.js
+++ packages/igmoweb-theme/src/components/archive/archive-title.js
@@ -1,20 +1,26 @@
+import React from 'react';
 import { connect, decode, styled } from 'frontity';
 
 const ArchiveTitle = ( { state } ) => {
 	const data = state.source.get( state.router.link );
-	const { id, isTaxonomy, taxonomy } = data;
+	const { id, isSearch, isTaxonomy, searchQuery, taxonomy } = data;
 	const { source } = state;
 
-	if ( ! isTaxonomy ) {
-		return null;
+	let title = '';
+	if ( isTaxonomy ) {
+		title = (
+			<>
+				{ taxonomy } =&gt;{ ' ' }
+				<strong>{ decode( source[ taxonomy ][ id ].name ) }</strong>
+			</>
+		);
+	} else if ( isSearch ) {
+		title = <>Search =&gt; { searchQuery }</>;
 	}
 
 	return (
 		<div>
-			<StyledArchiveTitle>
-				{ taxonomy } =&gt;{ ' ' }
-				<strong>{ decode( source[ taxonomy ][ id ].name ) }</strong>
-			</StyledArchiveTitle>
+			<StyledArchiveTitle>{ title }</StyledArchiveTitle>
 		</div>
 	);
 };
