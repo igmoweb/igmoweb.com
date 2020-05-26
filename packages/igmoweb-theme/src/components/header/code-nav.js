9e52c64206c4dff59af2000c8ebb9b20e19e5c43
Ignacio Cruz <igmoweb@gmail.com>
2020-05-26 11:20:35 +0200

    Nices code menu styles

--- packages/igmoweb-theme/src/components/header/code-nav.js
+++ packages/igmoweb-theme/src/components/header/code-nav.js
@@ -1,6 +1,6 @@
 import Link from '../common/link';
-import config from '../../config';
 import React from 'react';
+import config from '../../config';
 import { connect, styled } from 'frontity';
 
 const { fontSizes } = config;
@@ -26,8 +26,13 @@ const StyledList = styled.ul`
 	display: flex;
 	justify-content: flex-end;
 	font-size: ${fontSizes.small};
+	align-items: baseline;
 	li {
 		list-style: none;
+		:before {
+			content: '->';
+			margin-right: .5rem;
+		}
 		:first-of-type {
 			margin-left: 0;
 		}
