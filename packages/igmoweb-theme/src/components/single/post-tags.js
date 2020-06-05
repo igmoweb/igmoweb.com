f06c07e68d8b750aa228487551c6dc59e2f9331a
Ignacio Cruz <igmoweb@gmail.com>
2020-06-05 11:34:54 +0200

    Some styling issues fixed

--- packages/igmoweb-theme/src/components/single/post-tags.js
+++ packages/igmoweb-theme/src/components/single/post-tags.js
@@ -3,6 +3,7 @@ import React from 'react';
 import _get from 'lodash/get';
 import { badge } from '../../styles';
 import config from '../../config';
+import { mq } from '../../styles';
 import { connect, decode, styled } from 'frontity';
 
 const { fontSizes } = config;
@@ -38,14 +39,25 @@ export default connect( ( { post, state } ) => {
 } );
 
 const TagsList = styled.ul`
+	flex-wrap: wrap;
 	font-size: ${fontSizes.xsmall};
 	list-style: none;
 	display: flex;
 	margin-left: 0;
+	margin-bottom: 0;
 	li {
 		display: flex;
 		align-items: stretch;
 		margin: 0;
+		margin-bottom: 1rem;
 		margin-right: 1rem;
+	}
+
+	${mq( 'medium' )} {
+		flex-wrap: nowrap;
+		margin-bottom: unset;
+		li {
+			margin-bottom: 0;
+		}
 	}
 `;
